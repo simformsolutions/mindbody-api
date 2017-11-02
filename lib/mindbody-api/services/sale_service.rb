b66b03ad92b24616005d9fdce3946a31a7a5c08d
Steve <stevysmith.ss@gmail.com>
2017-11-02 00:08:45 +0000

    Add update services

--- lib/mindbody-api/services/sale_service.rb
+++ lib/mindbody-api/services/sale_service.rb
@@ -7,6 +7,7 @@ module MindBody
       operation :get_accepted_card_type,      :locals => false
       operation :get_sales
       operation :get_services
+      operation :update_services,   :required => [:services]
       operation :get_packages
       operation :get_products
       operation :get_custom_payment_methods
--- spec/services/sale_service_spec.rb
+++ spec/services/sale_service_spec.rb
@@ -7,6 +7,7 @@ describe MindBody::Services::SaleService do
   it { should respond_to(:get_accepted_card_type) }
   it { should respond_to(:get_sales) }
   it { should respond_to(:get_services) }
+  it { should respond_to(:update_services) }
   it { should respond_to(:get_packages) }
   it { should respond_to(:get_products) }
   it { should respond_to(:get_custom_payment_methods) }
