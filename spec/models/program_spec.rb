41370b0b69b08abeef3decbdaca076260c60f15f
Steve <stevysmith.ss@gmail.com>
2018-02-15 12:11:48 +0000

    Add cancel offset as attribute to program

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -4,4 +4,5 @@ describe MindBody::Models::Program do
   it { should respond_to(:id) }
   it { should respond_to(:name) }
   it { should respond_to(:schedule_type) }
+  it { should respond_to(:cancel_offset) }
 end
